ce1d09e667efb76339b63742dbe4c3598132b82c
tarkomate99 <tarkomate99@gmail.com>
2022-03-22 10:21:18 +0100

    pizzaproject create

---
+++ PizzaProject/PizzaProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PizzaProject
+//
+//  Created by Student on 2022. 03. 22..
+//  Copyright © 2022. vemwaat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
